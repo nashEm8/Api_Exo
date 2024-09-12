3c2d704ddc1219c0be82fc3946e82baee90c0a63
nashEm8 <emanuel82016@outlook.com>
2024-09-11 23:23:45 -0300

    Criando entidade usuario

--- Contexts/ExoContext.cs
+++ Contexts/ExoContext.cs
@@ -26,6 +26,7 @@ namespace Exo.WebAPi.Contexts
           }
         }
         public DbSet<Projeto> Projetos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
     }
 
 }
---
+++ Models/Usuario.cs
@@ -0,0 +1,14 @@
+namespace Exo.WebAPi
+{
+
+  public class Usuario 
+  {
+    
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+
+  }
+
+
+}
